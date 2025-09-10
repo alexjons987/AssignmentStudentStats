417fb7b5f92205e0ccf9687b08501d3e7a1b898e
alexjons987 <alexander.jonsson987@outlook.com>
2025-09-10 12:07:33 +0200

    Add final extra part of assigment, Update README.md

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Ni ska skriva ett Java-program som hanterar en klass med elever och deras provre
 
 ## Uppgifter att lösa
 ### Skapa data
-* Ha en lista/array med namn på elever (minst 8 st).
-* Ha en lista/array med motsvarande provresultat (0–100 poäng).
+* Ha en lista/array med namn på elever (minst 8 st). ✅
+* Ha en lista/array med motsvarande provresultat (0–100 poäng). ✅
 ### Beräkna statistik
 * Medelvärde av resultaten. ✅
 * Högsta och lägsta resultatet. ✅
 * Namnet på den elev som fick högst respektive lägst resultat. ✅
 ### Extra (för de som hinner)
-* Sortera eleverna efter resultat (stigande eller fallande). ❌
-* Lista alla elever som fick över medel. ❌
-* Låt användaren söka efter en elevs namn och skriva ut resultatet. ❌
-* komplettera resultaten med betyg (ex 80-100 = A, 0-40 = F, etc) ❌
+* Sortera eleverna efter resultat (stigande eller fallande). ✅
+* Lista alla elever som fick över medel. ✅
+* Låt användaren söka efter en elevs namn och skriva ut resultatet. ✅
+* komplettera resultaten med betyg (ex 80-100 = A, 0-40 = F, etc) ✅
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        int classSize = 8;
+        final int CLASS_SIZE = 8;
 
         // Create relevant arrays of relevant size
-        String[] students = new String[classSize];
-        int[] results = new int[classSize];
+        String[] students = new String[CLASS_SIZE];
+        int[] results = new int[CLASS_SIZE];
 
         // Add students
         students[0] = "Alexander";
@@ -32,7 +34,7 @@ public class Main {
         double totalResult = 0;
         for (int result : results)
             totalResult += result;
-        double averageResult = totalResult / (float) classSize;
+        double averageResult = totalResult / (float) CLASS_SIZE;
         System.out.printf("Average result: %.2f\n", averageResult);
 
         // Find the highest and lowest results
@@ -53,5 +55,70 @@ public class Main {
         }
         System.out.printf("Best student was %s with a score of %d\n", students[highestIndex], highestResult);
         System.out.printf("Worst student was %s with a score of %d\n", students[lowestIndex], lowestResult);
+
+        // Sort list based on result
+        for (int i = 0; i < CLASS_SIZE; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < CLASS_SIZE - i - 1; j++) {
+                if (results[j] < results[j + 1]) {
+                    String tempName = students[j];
+                    int tempScore = results[j];
+
+                    students[j] = students[j + 1];
+                    results[j] = results[j + 1];
+
+                    students[j + 1] = tempName;
+                    results[j + 1] = tempScore;
+
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.printf("%d. %s - %d (%c)\n", i + 1, students[i], results[i], getGradeFromResult(results[i]));
+        }
+
+        // Every student above average
+        int studentsAboveAvgCount = 0;
+        for (int i = 0; i < CLASS_SIZE; i++) {
+            if (results[i] > averageResult) {
+                System.out.printf("%s performed above average!\n", students[i]);
+                studentsAboveAvgCount++;
+            }
+        }
+        System.out.printf("Students above average: %d\n", studentsAboveAvgCount);
+
+        // Search-loop
+        while (true) {
+            String search;
+            do {
+                System.out.print("Search for a student: ");
+            } while ((search = scanner.next()).isEmpty());
+
+            for (int i = 0; i < CLASS_SIZE; i++)
+                if (students[i].contains(search))
+                    System.out.printf("%s - %d (%c)\n", students[i], results[i], getGradeFromResult(results[i]));
+
+            if (search.equals("exit"))
+                break;
+        }
+    }
+
+    public static char getGradeFromResult(int result) {
+        if (result <= 40)
+            return 'F';
+        else if (result <= 49)
+            return 'E';
+        else if (result <= 59)
+            return 'D';
+        else if (result <= 69)
+            return 'C';
+        else if (result <= 79)
+            return 'B';
+        else
+            return 'A';
     }
 }
